928184d89283ce5812a50e832cfaedf796f76ee0
joaoCrulhas <joao.crulhas@gmail.com>
2022-11-28 22:34:25 +0100

    refactor(Controller): validate required fields

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -7,6 +7,38 @@ import { HttpRequest, HttpResponse } from '../protocols';
 import { Controller } from './controller.protocol';
 
 class SignupController implements Controller {
+  private requiredFields: string[] = [];
+  constructor() {
+    this.requiredFields = [
+      'email',
+      'password',
+      'passwordConfirmation',
+      'username'
+    ];
+  }
+
+  private validateRequest(body: any): {
+    isValid: boolean;
+    missingParam?: string;
+  } {
+    let isValid = true;
+    let missingParam = '';
+    this.requiredFields.forEach((requiredFiled) => {
+      const hasProperty = Object.prototype.hasOwnProperty.call(
+        body,
+        requiredFiled
+      );
+      if (!hasProperty) {
+        isValid = false;
+        missingParam = requiredFiled;
+      }
+    });
+    return {
+      isValid,
+      missingParam
+    };
+  }
+
   exec({ body }: HttpRequest): HttpResponse {
     if (!body.email) {
       return badRequest(new MissingParamError('email'));
