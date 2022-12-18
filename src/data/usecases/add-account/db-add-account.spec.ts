70fb2e4ffd55326bfb168d2701458cde9457f3e2
joaoCrulhas <joao.crulhas@gmail.com>
2022-12-18 11:55:46 +0100

    test: encrypter should throw an execption if throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,8 +1,16 @@
-import { AddAccountDTO } from '../../../domain/use-cases/add-account.usecase';
+import {
+  AddAccount,
+  AddAccountDTO
+} from '../../../domain/use-cases/add-account.usecase';
 import { Encrypter } from '../../protocols/encrypter';
 import { DbAddAccount } from './db-add-account';
 
-const makeSut = () => {
+interface SystemUnderTest {
+  sut: AddAccount;
+  encrypter: Encrypter;
+}
+
+const makeSut = (): SystemUnderTest => {
   class EncrypterStub implements Encrypter {
     async encrypt(value: string): Promise<string> {
       return Promise.resolve('encrypted_string');
@@ -28,5 +36,19 @@ describe('DbAccount', () => {
     await sut.exec(request);
     expect(aSpy).toBeCalled();
     expect(aSpy).toBeCalledWith('valid_password');
+  });
+
+  it('Should throws an execption if encrypter throws an error', async () => {
+    const request: AddAccountDTO = {
+      email: 'valid_emaal@gmail.com',
+      username: 'username',
+      password: 'valid_password'
+    };
+    const { sut, encrypter } = makeSut();
+    jest.spyOn(encrypter, 'encrypt').mockImplementationOnce(() => {
+      return Promise.reject(new Error());
+    });
+    const promise = await sut.exec(request);
+    await expect(promise).rejects.toThrow();
   });
 });
