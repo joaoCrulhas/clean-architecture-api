9a960b9929689b82d009a3cb2e0fdfc164152f94
joaoCrulhas <joao.crulhas@gmail.com>
2023-01-08 20:22:26 +0100

    chore: add express and nomdeon

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,2 +1,7 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
+const app: Express = express();
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Running ${port}`);
+});
 console.log(1);
